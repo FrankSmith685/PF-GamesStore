39a7b83fc8c97cb1ab7b8e2fa3fb02abd818cc0f
12frankporx <f.s.b.rojas@gmail.com>
2022-12-16 01:28:11 -0500

    se modifico front

--- Client/src/index.js
+++ Client/src/index.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 import dotenv from "dotenv";
 
 dotenv.config();
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pf-gamesstore-production.up.railway.app";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
